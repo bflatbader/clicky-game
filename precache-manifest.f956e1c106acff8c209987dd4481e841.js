08397cd8dfaa2bf08dded8d0ede288055ec41d80
Bishop Bader <bflatbader@users.noreply.github.com>
2019-12-21 13:43:29 -0500

    Updates

---
+++ precache-manifest.f956e1c106acff8c209987dd4481e841.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a5558d69e4371e3155523133699bf2",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "17b187d05871e42c4710",
+    "url": "/clicky-game/static/css/main.2fadc7fa.chunk.css"
+  },
+  {
+    "revision": "687dbad3ae26bb0b5ac6",
+    "url": "/clicky-game/static/js/2.23d9921a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.23d9921a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "17b187d05871e42c4710",
+    "url": "/clicky-game/static/js/main.f3ebdf82.chunk.js"
+  },
+  {
+    "revision": "b859e8c835d6a54c0ef8",
+    "url": "/clicky-game/static/js/runtime-main.bf081868.js"
+  },
+  {
+    "revision": "8b38f43c8ff0c7b2c59d7209f9283b89",
+    "url": "/clicky-game/static/media/logo.8b38f43c.png"
+  }
+]);
